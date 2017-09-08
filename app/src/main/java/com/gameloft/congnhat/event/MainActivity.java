885aaa145437372b87e7e0748a13442dce7b4c7c
Cong Nhat <congnhat.234@gmail.com>
2017-09-08 13:29:27 +0700

    Initial commit

---
+++ app/src/main/java/com/gameloft/congnhat/event/MainActivity.java
@@ -0,0 +1,51 @@
+package com.gameloft.congnhat.event;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends Activity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btn2 = (Button) findViewById(R.id.btn2);
+        btn2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick (View arg0){
+                EditText edtA = (EditText)findViewById(R.id.edt_a);
+                int a = Integer.parseInt(edtA.getText() + "");
+                EditText edtB = (EditText)findViewById(R.id.edt_b);
+                int b = Integer.parseInt(edtB.getText() + "");
+                TextView result = (TextView) findViewById(R.id.result);
+                result.setText(a + b + "");
+            }
+        });
+        Button btn3 = (Button) findViewById(R.id.btn3);
+        btn3.setOnClickListener(this);
+    }
+        public void btn_tong(View v){
+            EditText edtA = (EditText)findViewById(R.id.edt_a);
+            int a = Integer.parseInt(edtA.getText() + "");
+            EditText edtB = (EditText)findViewById(R.id.edt_b);
+            int b = Integer.parseInt(edtB.getText() + "");
+            TextView result = (TextView) findViewById(R.id.result);
+            result.setText(a + b + "");
+    }
+
+    @Override
+    public void onClick(View view) {
+        EditText edtA = (EditText)findViewById(R.id.edt_a);
+        int a = Integer.parseInt(edtA.getText() + "");
+        EditText edtB = (EditText)findViewById(R.id.edt_b);
+        int b = Integer.parseInt(edtB.getText() + "");
+        TextView result = (TextView) findViewById(R.id.result);
+        result.setText(a + b + "");
+    }
+}
